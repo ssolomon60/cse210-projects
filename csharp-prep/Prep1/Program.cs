3e7e14347f34298993d4e26ded8a84882def77fd
ssolomon60 <sydney.solomon60@gmail.com>
2024-09-21 23:42:07 -0600

    Update Program.cs
    I had to copy and paste it from VS Code, i haven't figured out how to push it

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,5 @@
-using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
-}
+Console.Write("Enter your first name: ");
+        string firstName = Console.ReadLine();   
+Console.Write("Enter your last name: ");
+        string lastName = Console.ReadLine();
+Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
