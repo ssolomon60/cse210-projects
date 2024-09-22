85db9428969d50d6960b2d848bf25f2044786516
ssolomon60 <sydney.solomon60@gmail.com>
2024-09-21 23:45:00 -0600

    Update Program.cs
    Had to copy and paste from VS code because I haven't figured out how to push code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+                Random random = new Random();
+        string playAgain;
+
+        do
+        {
+            int magicNumber = random.Next(1, 101), guessCount = 0, guess;
+            Console.WriteLine("I have picked a magic number between 1 and 100. Try to guess it!");
+
+            do
+            {
+                Console.Write("Your guess: ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                Console.WriteLine(guess < magicNumber ? "Higher" : guess > magicNumber ? "Lower" : $"You guessed it in {guessCount} guesses!");
+
+            } while (guess != magicNumber);
+
+            Console.Write("Play again? (yes/no): ");
+            playAgain = Console.ReadLine().Trim().ToLower();
+
+        } while (playAgain == "yes");
+
+        Console.WriteLine("Thanks for playing! Goodbye.");
     }
 }
