70a5fe636853c42c7fa7a26e1ffb00c912ac9cca
ssolomon60 <sydney.solomon60@gmail.com>
2024-09-21 23:43:48 -0600

    Update Program.cs
    I had to Copy and paste from VS Code. I haven't figured out how to push it

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+                Console.Write("Enter your grade percentage: ");
+        int percentage = int.Parse(Console.ReadLine());
+        string letter = "";
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is: {letter}");
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up, keep trying next time!");
+        }
     }
 }
