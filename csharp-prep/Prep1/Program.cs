e6f60f2d19327c22165840432dcb298534fe14d5
ssolomon60 <sydney.solomon60@gmail.com>
2024-09-25 10:31:32 -0600

    Finished program prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,5 +1,12 @@
-Console.Write("Enter your first name: ");
-        string firstName = Console.ReadLine();   
-Console.Write("Enter your last name: ");
-        string lastName = Console.ReadLine();
-Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+class Program
+{
+        static void Main(string[] args)
+        {
+                Console.WriteLine("What is your first name: ");
+                string first_name  = Console.ReadLine();
+                Console.WriteLine("What is your last name: ");
+                string last_name = Console.ReadLine();
+                Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+
+        }
+}
