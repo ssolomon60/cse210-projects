97e68865d30b016b6ecd2ec9c32dde761dccaf15
ssolomon60 <sydney.solomon60@gmail.com>
2024-10-10 09:43:11 -0600

    Foundation 1

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -1,9 +1,54 @@
 using System;
 
+class video
+{
+    public string title;
+    public string author;
+    public int length;
+    public List<Comments> comments = new List<Comments>();
+    public video(string _title, string _author, int _length, int _comments)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+    }
+    public void GetComments()
+    {
+        return comments;
+    }
+    public int GetCommentCount()
+    {
+        return comments.Count;
+    }
+}
+class Comments
+{
+    public string CommentName;
+    public string text;
+    public Comments(string _CommentName, string _text)
+    {
+        _CommentName = CommentName;
+        _text = text;
+    }
+}
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        List<video> videos = new List<video>();
+        foreach (video video in videos) 
+        {
+            Console.WriteLine($"Title: {video.title}");
+            Console.WriteLine($"Author: {video.author}");
+            Console.WriteLine($"Length: {video.length}");
+            Console.WriteLine($"Comment Count: {video.GetCommentCount()}")
+
+            Console.WriteLine($"Comments:")
+            foreach (Comments comments in comments)
+            {
+                Console.WriteLine($"{CommentName}: {text}")
+            }
+
+        }
     }
 }
