bdef8f9888549c8dff4f233618c7ac35d0838c4c
ssolomon60 <sydney.solomon60@gmail.com>
2024-10-12 15:24:17 -0600

    Foundation 2

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -1,9 +1,62 @@
 using System;
 
+class Video
+{
+    public string title;
+    public string author;
+    public int length;
+    public List<Comments> comments = new List<Comments>();
+    public Video(string _title, string _author, int _length)
+    {
+        title = _title;
+        author = _author;
+        length = _length;
+    }
+    public List<Comments> GetComments()
+    {
+        return comments;
+    }
+    public void AddComments(Comments comment)
+    {
+        comments.Add(comment);
+    }
+    public int GetCommentCount()
+    {
+        return comments.Count;
+    }
+}
+class Comments
+{
+    public string CommentName;
+    public string text;
+    public Comments(string _CommentName, string _text)
+    {
+        CommentName =_CommentName;
+        text = _text;
+    }
+}
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        List<Video> videos = new List<Video>();
+        Video video_1 = new Video("Minecraft Stories Part 1", "Cheesypie", 625);
+        video_1.AddComments(new Comments("George", "What a wack video yo! How you going to let my character die like that"));
+
+        videos.Add(video_1);
+        foreach (Video video in videos) 
+        {
+            Console.WriteLine($"Title: {video.title}");
+            Console.WriteLine($"Author: {video.author}");
+            Console.WriteLine($"Length: {video.length} in seconds");
+            Console.WriteLine($"Comment Count: {video.GetCommentCount()}");
+
+            Console.WriteLine($"--Comments--");
+            foreach (Comments comment in video.GetComments())
+            {
+                Console.WriteLine($"{comment.CommentName}: {comment.text}");
+            }
+
+        }
     }
 }
--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -1,9 +1,157 @@
 using System;
+using System.Collections.Generic;
+
+class Product
+{
+    private string _name;
+    private string _productId;
+    private double _price;
+    private int _quantity;
+
+    public Product(string name, string productId, double price, int quantity)
+    {
+        _name = name;
+        _productId = productId;
+        _price = price;
+        _quantity = quantity;
+    }
+
+    public double GetTotalCost()
+    {
+        return _price * _quantity;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public string GetProductId()
+    {
+        return _productId;
+    }
+}
+
+class Customer
+{
+    private string _name;
+    private Address _address;
+
+    public Customer(string name, Address address)
+    {
+        _name = name;
+        _address = address;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public Address GetAddress()
+    {
+        return _address;
+    }
+
+    public bool IsInUSA()
+    {
+        return _address.IsInUSA();
+    }
+}
+
+class Address
+{
+    private string _street;
+    private string _city;
+    private string _state;
+    private string _country;
+
+    public Address(string street, string city, string state, string country)
+    {
+        _street = street;
+        _city = city;
+        _state = state;
+        _country = country;
+    }
+
+    public bool IsInUSA()
+    {
+        return _country.ToLower() == "usa";
+    }
+
+    public string GetFullAddress()
+    {
+        return $"{_street}\n{_city}, {_state}\n{_country}";
+    }
+}
+
+class Order
+{
+    private List<Product> _products;
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+        _products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public double GetTotalPrice()
+    {
+        double total = 0;
+
+        foreach (Product product in _products)
+        {
+            total += product.GetTotalCost();
+        }
+
+        if (_customer.IsInUSA())
+        {
+            total += 5.00;
+        }
+
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "Packing Label:\n";
+        foreach (Product product in _products)
+        {
+            label += $"{product.GetName()} (ID: {product.GetProductId()})\n";
+        }
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"Shipping Label:\n{_customer.GetName()}\n{_customer.GetAddress().GetFullAddress()}";
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+
+        Product product1 = new Product("Apples", "App1", 0.99, 1);
+        Product product2 = new Product("Bananas", "Ban1", 0.49, 1);
+        Product product3 = new Product("Oranges", "Ora1", 0.89, 1);
+        Address address1 = new Address("120 Main Street", "Rexburg", "ID", "USA");
+        Customer customer1 = new Customer("Sydney Solomon", address1);
+        Order order1 = new Order(customer1);
+        order1.AddProduct(product1);
+        order1.AddProduct(product2);
+        order1.AddProduct(product3);
+        Console.WriteLine("Order 1: ");
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine("Total Price: $" + order1.GetTotalPrice());
+        Console.WriteLine();
     }
 }
